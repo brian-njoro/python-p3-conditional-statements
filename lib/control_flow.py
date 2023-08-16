84e5e1abf4f11e76ba625135a0cabc9815b9adac
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-08-16 13:41:55 +0300

    done with assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if (username == 'admin' or username == 'ADMIN') and password = '12345':
+        return 'Access Granted'
+    else:
+        return "Access Denied"   
+    
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out here"
+    if (temperature>=40 and temperature<=65):
+        return "It's a bit chilly out here"
+    else:
+        return "It's perfect out here"
+
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    elif num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    else:
+        return num
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        return "Invalid operation"
